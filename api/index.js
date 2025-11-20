72233e8c4ec8ea2ea2cfd8b491fbbcd4db4b48f0
dev-shahbaz-ali <devshahbazali.it@gmail.com>
2025-11-20 16:02:06 +0500

    feat: restructure application to move index.js to api directory and update package scripts

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import serverless from "serverless-http";
+import ContactRoutes from "../routes/contacts.routes.js";
+import { connectDB } from "../config/database.js";
+
+connectDB();
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+app.use("/api", ContactRoutes);
+
+export default serverless(app);
