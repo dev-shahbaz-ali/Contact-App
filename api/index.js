b8c2e5a198704c36c1aa33a3740006b6d0a1293d
dev-shahbaz-ali <devshahbazali.it@gmail.com>
2025-11-20 20:58:33 +0500

    change in in vercel.josn file remove build type

--- api/index.js
+++ api/index.js
@@ -6,10 +6,11 @@ import { connectDB } from "../config/database.js";
 connectDB();
 
 const app = express();
-app.set("view engine", "ejs");
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.json());
-app.use("/", ContactRoutes);
 
-export default serverless(app);
+app.use("/api", ContactRoutes);
+
+export const handler = serverless(app);
