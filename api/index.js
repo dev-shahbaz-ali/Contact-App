7f92c225027ade52d0bf2671ded1c2b93e82c406
dev-shahbaz-ali <devshahbazali.it@gmail.com>
2025-11-20 16:13:33 +0500

    remove api key in index.js file

--- api/index.js
+++ api/index.js
@@ -10,6 +10,6 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
-app.use("/api", ContactRoutes);
+app.use("/", ContactRoutes);
 
 export default serverless(app);
