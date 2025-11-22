a7175dae61025c7e282652baf480621d976d6830
dev-shahbaz-ali <devshahbazali.it@gmail.com>
2025-11-22 10:30:21 +0500

    Restore original working code and fix numbring and prev page and next page settings add if condition version 3

--- controller/contacts.controller.js
+++ controller/contacts.controller.js
@@ -8,7 +8,7 @@ const getContacts = async (req, res) => {
       contacts: contacts,
       totalDocs: contacts.length,
       limit: 10,
-      totalPages: contacts.length > 0 ? 1 : 0,
+      totalPages: 1,
       currentPage: 1,
       counter: 1,
       hasPrevPage: false,
